c0ed09ed876355a7fdd578047ee6f9d110ccf3ee
yeyuanzhiye <2143475283@qq.com>
2024-08-31 12:53:45 +0800

    day1

---
+++ test/test_8_25/test_8_25/test.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//
+// //int main()
+//{
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//	printf("呆滞一个，一个呆滞\n");
+//
+//		return 0;
+//}
+//short main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+//short main()
+//{
+//	int size = 20;
+//	float trueszie = 1.1;
+//	return 0;
+//}
+//int a = 666;
+//int main()
+//{
+//	printf("陈逸=%d\n",a);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//
+//	scanf("%d %d", &num1, &num2);//输入
+//
+//	int sum = num1 + num2;//求和
+//
+//	printf("%d\n", sum);//输出
+//
+//	return 0;
+//}
+//extern int a;
+//void test()
+//	{
+//	printf("test-->%d\n", a);
+//	}
+//
+//int main()
+//{
+//	test();
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	const int a = 10;
+//	a = 20;
+//	printf("%d\n", a);*/
+//	const int n = 10;
+//	int arr[10] = { 0 };
+//	return 0;
+//}
+//#define A 100
+//#define B "abc"
+//int main()
+//{
+//	printf("%d\n",A);
+//	printf("%s\n",B);
+//	return 0;
+//}
+//enum Color
+//{
+//	RED,
+//	GREEN,
+//	BLUE,
+//};
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET,
+//};
+//int main()
+//{
+//	enum Color c = RED;
+//	printf("%d\n", MALE);
+//	printf("%d\n", c);
+//	printf("%d\n", c);
+//	return 0;
+//}
